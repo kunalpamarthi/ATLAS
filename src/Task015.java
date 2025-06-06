aba5a4509e7ca324d7aa2838213e706a1f6fde02
kunalLocal <kunal@local.win>
2025-06-06 16:03:13 +0530

    Exploring Strings.

---
+++ src/Task015.java
@@ -0,0 +1,13 @@
+// Task015: Strings
+
+public class Task015 {
+    public static void main(String[] args) {
+        String str1 = "Java Strings "; // string Literal
+        String str2 = new String(str1); // obj of the string - new keyword
+        String str3 = new String("are easy to learn ");
+        char[] ch = {'S', 't', 'r' ,'i', 'n', 'g'};
+        String str4 = new String(ch);
+        System.out.println(str1 + "\n" + str2+str3 + "\n" +str4);
+
+    }
+}

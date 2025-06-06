bdfdc2d3b66dacbf1db9ad7c64255a1c1101f62b
kunalLocal <kunal@local.win>
2025-06-06 16:59:52 +0530

    Exploring Getters and Setters.

---
+++ src/Task018.java
@@ -0,0 +1,9 @@
+/*Task018: Now create one more program named Task018.java
+Now think what is the output of the below code.*/
+
+public class Task018 {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+    }
+}

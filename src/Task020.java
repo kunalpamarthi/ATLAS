2e5b45ab21bd176ae8936e56b6c922617df214e6
kunalLocal <kunal@local.win>
2025-06-06 18:12:13 +0530

    Exploring enumerations.

---
+++ src/Task020.java
@@ -0,0 +1,13 @@
+//Task020: Create an array of your name.
+
+public class Task020 {
+    public static void main(String[] args) {
+        char[] name = {'k', 'u', 'n', 'a', 'l'};
+        System.out.println(name);
+        int nameLength = name.length;
+        System.out.println("There are "+nameLength+" letters in my name.");
+        for(int i = 0; i < nameLength; i++) {
+            System.out.print(name[i]+ " ");
+        }
+    }
+}

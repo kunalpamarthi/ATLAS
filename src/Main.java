945cac4e7d900183f757fcfef78988ec6fc5eaa9
kunalLocal <kunal@local.win>
2025-06-07 10:49:40 +0530

    Test file: checking toString()

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+class Student{
+   public String name;
+   public String mobile;
+    Student(String name, String mobile){
+       this.name = name;
+       this.mobile = mobile;
+   }
+
+    @Override
+    public String toString() {
+        return name+" "+mobile;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hello, everyone! welcome ATLAS training.");
-            System.out.println("In today's session we are going to explore java and its operations");
+        Student[] myStudents = new Student[]{
+                new Student("Dharma", "123"), new Student("Sanvi", "456"),
+                new Student("Rupa", "345"), new Student("Ajay", "789")
+        };
+        // accessing the elements of the specified array
+        for (Student m : myStudents) {
+            System.out.println(m);
+        }
     }
 }

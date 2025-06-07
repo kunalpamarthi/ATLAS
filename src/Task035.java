09cf65f1dd20d157e08d2784b2480cf5c3c1261c
kunalLocal <kunal@local.win>
2025-06-07 16:42:25 +0530

    Polymorphism: add method with different types of parameters.

---
+++ src/Task035.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Task035 {
+    static void add(double a, double b){
+        System.out.println("Sum of given numbers: "+ (a+b));
+    }
+    static void add(char a, char b){
+        System.out.println("Sum of given numbers: "+ (a+b));
+    }
+    static void add(int a, int b){
+        System.out.println("Sum of given numbers: "+ (a+b));
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two numbers: ");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        add(a, b);
+        add((double) a, b);
+        add('a', 'b');
+
+        scanner.close();
+    }
+}

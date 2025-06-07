53edaea03b67fd8f8c96d7c2c5ec2bb99650a147
kunalLocal <kunal@local.win>
2025-06-07 21:44:48 +0530

    Interfaces: Working of interfaces in Java.

---
+++ src/Task041.java
@@ -0,0 +1,20 @@
+// Interfaces
+
+interface testInterface {
+    final int tax = 10;
+    void display();
+}
+
+class TestClass implements testInterface {
+    public void display(){
+        System.out.println("My class");
+        System.out.println("Tax value: "+tax);
+    }
+}
+
+public class Task041 {
+    public static void main(String[] args) {
+        TestClass testClass = new TestClass();
+        testClass.display();
+    }
+}

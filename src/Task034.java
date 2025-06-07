1cd692f8435ce495eb604b410e7b35f035fdc27e
kunalLocal <kunal@local.win>
2025-06-07 16:26:23 +0530

    Polymorphism: add method with different no. of parameters.

--- src/Task034.java
+++ src/Task034.java
@@ -7,15 +7,8 @@ public class Task034 {
         System.out.println("Sum of given numbers: "+ (a+b+c));
     }
 
-    static void add(double a, double b){
-        System.out.println("Sum of given numbers: "+ (a+b));
-    }
-
     public static void main(String[] args) {
         add(1, 2);
         add(1, 5, 6);
-        add(3.0, 10.0);
-
-
     }
 }

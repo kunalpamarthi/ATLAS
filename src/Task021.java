ce95e9d8b2153eb2827d761d7fb57d21dd597bed
kunalLocal <kunal@local.win>
2025-06-12 17:45:06 +0530

    Arrays: Initialize an array and traverse it.

---
+++ src/Task021.java
@@ -0,0 +1,11 @@
+// Task021: Initialize an array and traverse it.
+
+public class Task021 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int arrLen = arr.length;
+        for (int i = 0; i < arrLen; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}

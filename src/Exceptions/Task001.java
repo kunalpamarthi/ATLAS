9b9365de0c8173646d71c79adbfbd2a2aac394dc
kunalLocal <kunal@local.win>
2025-06-13 10:20:41 +0530

    Exception Handling: Access an element at index greater than the size of an array.

---
+++ src/Exceptions/Task001.java
@@ -0,0 +1,11 @@
+//Task001: Create an array of size 'n' and
+// try to access the element at the index of 'n+'.
+
+package Exceptions;
+
+public class Task001 {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3};
+        System.out.println(num[5]);
+    }
+}

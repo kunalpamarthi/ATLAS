0a16212f04f4f8690412ad59747795d55434bcef
kunalLocal <kunal@local.win>
2025-06-13 11:15:03 +0530

    Exception Handling: Nested try blocks.

---
+++ src/Exceptions/Task006.java
@@ -0,0 +1,24 @@
+// Task006: Nested try blocks.
+
+package Exceptions;
+
+public class Task006 {
+    public static void main(String[] args) {
+        try {
+            int[] a = new int[2];
+//            System.out.println("Access element three :" + a[2]);
+            System.out.println("Access element three :" + a[1]);
+            try {
+                int b = 0;
+                int c = 1/b;
+            }catch(Exception e) {
+                System.out.println("Exception thrown: " + e);
+            }
+            System.out.println("Access element three :" + a[3]);
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception thrown: " + e);
+        }
+        System.out.println("Out of the block");
+    }
+}

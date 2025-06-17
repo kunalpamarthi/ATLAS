1b7a33e719ce7d1c26665de809bc607f9587ef83
kunalLocal <kunal@local.win>
2025-06-17 20:00:52 +0530

    Arrays: Multidimensional.

---
+++ src/Task025.java
@@ -0,0 +1,9 @@
+// Task025: Multidimensional arrays.
+
+public class Task025 {
+    public static void main(String[] args) {
+        int[][] array = new int[3][4];
+        System.out.println("Rows: "+array.length);
+        System.out.println("Columns: "+array[0].length);
+    }
+}

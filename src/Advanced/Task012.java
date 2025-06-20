300f4cecc3b11cc2ef005fc7b44294a7e328df3c
kunalLocal <kunal@local.win>
2025-06-20 16:13:20 +0530

    Double colon(::).

---
+++ src/Advanced/Task012.java
@@ -0,0 +1,13 @@
+// Double colon
+
+package Advanced;
+
+import java.util.stream.Stream;
+
+public class Task012 {
+    public static void main(String[] args) {
+        Stream <String> stream = Stream.of("shiro",
+                "is", "very", "good", "boy.");
+        stream.forEach(System.out::println);
+    }
+}

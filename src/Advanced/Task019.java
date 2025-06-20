135dcec31209c91c61cbd674d0551b97a909bcd9
kunalLocal <kunal@local.win>
2025-06-20 21:30:03 +0530

    Exploring threads.

---
+++ src/Advanced/Task019.java
@@ -0,0 +1,26 @@
+// Task019: Exploring threads.
+
+package Advanced;
+
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Code executed in a new thread via Runnable.");
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Code executed in a new thread via Thread extension.");
+    }
+}
+class Task019 {
+    public static void main(String[] args) {
+        MyRunnable runnableInstance = new MyRunnable();
+        MyThread threadInstance = new MyThread();
+        Thread t1 = new Thread(runnableInstance);
+        t1.start(); // For MyRunnable
+        threadInstance.start(); // For MyThread
+    }
+}

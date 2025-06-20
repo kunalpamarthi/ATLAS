338272f61ba2889dfa54cd2b89e68dbf60987900
kunalLocal <kunal@local.win>
2025-06-20 13:30:00 +0530

    Threads Synchronization

---
+++ src/Advanced/Task006.java
@@ -0,0 +1,50 @@
+// Task006: Threads Synchronization
+
+package Advanced;
+
+class SyncCounter {
+    private int count = 0;
+
+    public void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+class SyncThreadDemo extends Thread {
+    SyncCounter counter;
+
+    SyncThreadDemo(SyncCounter counter) {
+        this.counter = counter;
+    }
+
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            counter.increment();
+        }
+    }
+}
+
+public class Task006 {
+    public static void main(String[] args) {
+        SyncCounter counter = new SyncCounter();
+        SyncThreadDemo t1 = new SyncThreadDemo(counter);
+        SyncThreadDemo t2 = new SyncThreadDemo(counter);
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            //noinspection CallToPrintStackTrace
+            e.printStackTrace();
+        }
+
+        System.out.println("Final count: " + counter.getCount());
+    }
+}

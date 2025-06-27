c2bb3dcee7b13ed92181696abe1f932a86f8e299
kunalLocal <kunal@local.win>
2025-06-27 11:19:10 +0530

    Added binary conversion code to Main.

--- Programming/DataStructures/src/Main.java
+++ Programming/DataStructures/src/Main.java
@@ -1,17 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-
         int x = 65;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter character to covert: ");
+        String chr = scanner.next();
+        x = chr.charAt(0);
         String bin = "";
         while (x>0){
             bin += String.valueOf((x%2));
             x /= 2;
         }
-        System.out.println("Binary of 65: "+ bin);
+        System.out.println("Binary of "+chr+": "+ bin);
+        scanner.close();
     }
 }

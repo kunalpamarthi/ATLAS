f4535d4a6aad5a61c10af7959f324d43734b14bd
kunalLocal <kunal@local.win>
2025-06-27 10:32:20 +0530

    Added binary conversion code to Main.

--- Programming/DataStructures/src/Main.java
+++ Programming/DataStructures/src/Main.java
@@ -6,10 +6,18 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+//        for (int i = 1; i <= 5; i++) {
+//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//            System.out.println("i = " + i);
+//        }
+
+        int x = 65;
+        String bin = "";
+        while (x>0){
+            bin += String.valueOf((x%2));
+            x /= 2;
         }
+        System.out.println("Binary of 65: "+ bin);
     }
 }

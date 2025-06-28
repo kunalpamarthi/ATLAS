052696c2673d09b49e70e721facb10c789464279
kunalLocal <kunal@local.win>
2025-06-28 16:02:32 +0530

    Predefined LinkedList.

---
+++ Programming/DataStructures/src/LinkedLists/Task006.java
@@ -0,0 +1,16 @@
+// Task006: Create linked list using Pre
+// defined class and add elements to it.
+
+package LinkedLists;
+
+import java.util.LinkedList;
+
+public class Task006 {
+    public static void main(String[] args) {
+        LinkedList<String> fruitList = new LinkedList<>();
+        fruitList.add("Mango");
+        fruitList.add("Apple");
+        fruitList.add("Banana");
+        System.out.println("Fruits list: "+ fruitList);
+    }
+}

e57fd78457ad40484170935f3f86be94e6962005
kunalLocal <kunal@local.win>
2025-07-01 07:51:43 +0530

    Space complexity.

--- Programming/coding_practice/SelfLearning/Java/src/DataStructures/DS_1_Index.java
+++ Programming/coding_practice/SelfLearning/Java/src/DataStructures/DS_1_Index.java
@@ -1,5 +1,7 @@
 package DataStructures;
 
+import java.util.ArrayList;
+
 public class DS_1_Index {
 
     public static void forLoopTime(){
@@ -14,11 +16,23 @@ public class DS_1_Index {
 
         String last = students[strength-1]; // O(1)
         System.out.println("\n"+first+"\t"+last); // O(1)
+
+        // Space complexity:
+        forLoopSpace(students);
     }
 
     public static void main(String[] args) {
         // How to calculate time complexity:
         forLoopTime(); // gives us: O(5 + n + n)
         // => O(5 + 2n) => O(2n) => O(n)
+
+    }
+
+    private static void forLoopSpace(String[] students) {
+        ArrayList<Integer> studentsNameLength = new ArrayList<>();
+        for (String student: students){
+            studentsNameLength.add(student.length());
+        }
+        System.out.println(studentsNameLength);
     }
 }

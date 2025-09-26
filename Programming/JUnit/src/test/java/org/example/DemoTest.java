c10e110cafeac719df7cfad4509e45992d06d4d3
kunalLocal <kunal@local.win>
2025-09-26 15:14:24 +0530

    JUnit Demo test

---
+++ Programming/JUnit/src/test/java/org/example/DemoTest.java
@@ -0,0 +1,20 @@
+// Task001: Demo test class.
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DemoTest {
+    @Test
+    public void Testcase1() {
+        String str = "shiro";
+        String name = "shiro";
+        assertEquals(str, name);
+    }
+}
+
+
+
+
+
+
